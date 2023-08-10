eefb7d0acae26ba2385ae1a95af904f9b83b7e57
Iamneo <techadmin@iamneo.ai>
2023-08-10 06:42:15 +0000

    Backup Commit

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -3,13 +3,12 @@ import './App.css';
 
 function App() {
   return (
-      <div className ="container">
-          <div className="App">
-                <Stopwatch />
-                    </div>
-                        </div>
-                          );
-                          }
+    <div className ="container">
+    <div className="App">
+      <Stopwatch />
+    </div>
+    </div>
+  );
+}
 
-                          export default App;
-                          
+export default App;
